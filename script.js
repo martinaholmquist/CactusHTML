a6e5d5ceeeb86fd3c769e85b4fee1a0fd5f0146a
Martina <73692480+martinaholmquist@users.noreply.github.com>
2023-01-07 13:03:24 +0100

    rensat upp

--- script.js
+++ script.js
@@ -8,33 +8,6 @@ function taFramKnapp(){
 menyKnappEl.addEventListener('click', taFramKnapp);
 
 
-/*
-
-
-document.getElementById("submitknapp").addEventListener("click", function(event){
-event.preventDefault()
-});
-
-
-const kontakt = document.getElementById("kontaktformular");
-const subknapp = document.getElementById("submitknapp"); 
-
-function hideformular(){
-    kontakt.classList.toggle("hideformular");
-}
-subknapp.addEventListener('click', hideformular);
-
-
-const kontakt2 = document.getElementById("kontaktformular");
-const subknapp2 = document.getElementById("submitknapp"); 
-function changeText() {
-    kontakt2.innerHTML = "Tack för ditt meddelande!";
-}
-
-subknapp2.addEventListener('click', changeText);
-
-*/
-
 
 
 var nextStep = document.querySelector('#submitknapp');
@@ -45,25 +18,9 @@ var nextStep = document.querySelector('#submitknapp');
     document.getElementById('kontaktformular').style.display = 'none';
 
     // Show thank you message element
-    document.getElementById('thank_you').style.display = 'block';
+    document.getElementById('thank_you').style.display = 'block'
   });
 
 
 
-
-
-/*
-
-  var nextStep = document.querySelector('#nextStep');
-
-  nextStep.addEventListener('click', function (e) {
-    e.preventDefault();
-    // Hide first view
-    document.getElementById('my_form').style.display = 'none';
-
-    // Show thank you message element
-    document.getElementById('thank_you').style.display = 'block';
-  });
-
-*/
 
